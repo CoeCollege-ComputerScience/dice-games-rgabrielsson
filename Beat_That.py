e6b90b1e4d38e5a79489a376e2f6a66df39b96e9
Rebecca Gabrielsson <rmgabrielsson23@coe.edu>
2025-03-13 16:02:24 +0000

    Beat That complete

--- Beat_That.py
+++ Beat_That.py
@@ -8,21 +8,42 @@ def roll_dice():
     else:
         return str(roll2)+str(roll1)
 
-def player_turn(first,second):
+
+def swap(active_player):
+    if active_player == "1":
+        return "2"
+    else:
+        return "1"
+
+
+#first and second should be "1" and "2" or "2" and "1"
+def round(starter):
+    active_player = starter
 
     print("")
-    print(f"Player {first} starts: ")
+    print(f"Player {active_player} starts: ")
     highest = roll_dice()
-    print(f"Player {first} rolls {highest}.")
+    print(f"Player {active_player} rolls {highest}")
 
 
-    
+    active_player = swap(active_player)
     new = roll_dice()
+    print(f"Player {active_player} rolls {new}")
     while new > highest:
         highest = new
+        active_player = swap(active_player)
         new = roll_dice()
-        print(f"Player {player} rolled {new}")
-    return highest
+        print(f"Player {active_player} rolls {new}")
+    
+    #in the event of a tie, no one gets any points
+    if new == highest:
+        print("Tie: no one wins")
+        return "0", highest
+
+    #return the winner and the score that they gain
+    active_player = swap(active_player)
+    print(f"Player {active_player} wins the round.")
+    return active_player, highest
 
 
 
@@ -37,15 +58,22 @@ def main():
 
     player1_score = 0
     player2_score = 0
+    active_player = "1"
     while player1_score < 100 and player2_score < 100:
-        
+        winner, round_total = round(active_player)
+        if winner == "1":
+            player1_score += int(round_total)
+        elif winner == "2":
+            player2_score += int(round_total)
+        print(f"Player 1 score: {player1_score}")
+        print(f"Player 2 score: {player2_score}")
+        active_player = swap(active_player)
 
 
-
-
+    print("")
     if player1_score >= 100:
         print("Player 1 wins!")
     else:
         print("Player 2 wins!")
 
-#main()
+main()
